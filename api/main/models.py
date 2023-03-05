dd0f2c647b3f75717eced1472d8b088b200be2f8
Trionyx <Trionyxxx@gmail.com>
2023-03-05 14:15:53 +0400

    BaseModelManager model filled

--- api/main/models.py
+++ api/main/models.py
@@ -33,7 +33,26 @@ class ScoolAdmin(BaseUser):
 
 
 class BaseModelManager(BaseUserManager):
-    pass
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError("Users must have email field")
+        if not username:
+            raise ValueError("Users must have username field")
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username, )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username=username,
+                                email=email,
+                                password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class BaseContent(models.Model):
