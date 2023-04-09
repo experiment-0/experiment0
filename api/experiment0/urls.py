846dab3843ddab68f3666a72fc8d8298b3c6fa3d
polinamalyhina <polja.malygina@gmail.com>
2023-04-09 12:56:01 +0300

    правка моделей, создание школ

--- api/experiment0/urls.py
+++ api/experiment0/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', view=include("user.urls"))
+    path('user/', view=include("user.urls")),
+    path('content/', view=include("school.urls"))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
