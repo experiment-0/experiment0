55499e1d6ebac45ab21c03961ae01decbe759e6f
polinamalyhina <polja.malygina@gmail.com>
2023-04-29 20:12:40 +0300

    логин

--- api/user/views.py
+++ api/user/views.py
@@ -1,16 +1,16 @@
 from django.contrib.auth.tokens import default_token_generator
+from django.contrib.auth import login, logout
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils import timezone
 from django.core.mail import send_mail
-from rest_framework import permissions, status, generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions, status, exceptions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import BaseUser
-from .serializers import BaseUserRegistrationSerializer
+from .serializers import BaseUserRegistrationSerializer, LoginSerializer
 
 
 class RegistrationAPIView(APIView):
@@ -58,3 +58,32 @@ class EmailVerificationAPIView(APIView):
             )
 
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        email = serializer.validated_data.get('email')
+        password = serializer.validated_data.get('password')
+        try:
+            user = BaseUser.objects.get(email=email)
+            if password == user.password:
+                login(request, user)
+                return Response({"email": email},
+                            status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Invalid email or password."},
+                            status=status.HTTP_401_UNAUTHORIZED)
+        except BaseUser.DoesNotExist:
+            raise exceptions.AuthenticationFailed('No such user')
+
+
+class LogoutAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({"message": "Logged out successfully."})
